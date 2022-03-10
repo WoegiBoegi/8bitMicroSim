82f8fe8506ead3a35a60f6b664ae5f29a4d909cd
BASICdev <elias.woeginger@gmail.com>
2022-03-10 12:55:48 +0100

    MVI and MVO instructions implemented (and a bunch more). Stack now functional and used by call and return.

--- main.c
+++ main.c
@@ -1,32 +1,50 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "BIOSSim.h"
+//0x0000 - 0x001F for stack (32 bytes)
+//0x0020 - 0x003F for interrupt handling (32 bytes)
+#define INTHANDSIZE 0x20
 #define MEMSIZE 0x100
-#define MEMFILEPATH "/home/woegi/OneDrive/Projects/CPUSim/mem.bin" //"C:\\Users\\woegi\\OneDrive\\Projects\\CPUSim\\mem.bin"
+#define STACKSIZE 0x20
+#define ENTRYPOINT (STACKSIZE + INTHANDSIZE)
+#define MEMFILEPATH "/home/woegi/OneDrive/Projects/8bitMicroSim/mem.bin" //"C:\\Users\\woegi\\OneDrive\\Projects\\CPUSim\\mem.bin"
 
 #define NOP  0x00   //no operation, do nothing
 #define HLT  0x0F   //halt execution
 #define LDA  0x2A   //load value into accumulator from memory address (specified in the next two memory addresses)
-#define LDAL 0x2B   //load value (in next Memory address) into Accumulator
+#define LDL  0x2B   //load value (in next Memory address) into Accumulator
 #define STA  0x2C   //store value from accumulator in memory address (specified in the next two memory addresses)
-#define LDAM 0x21   //load value (from Memory address specified in REG_H and REG_L) into ACC
-#define STAM 0x22   //store value from ACC in Memory address (specified in REG_H and REG_L)
+#define LDM  0x21   //load value (from Memory address specified in REG_H and REG_L) into ACC
+#define STM  0x22   //store value from ACC in Memory address (specified in REG_H and REG_L)
 #define JMP  0x70   //jumps to address
-#define JE   0x71   //jumps to address if zero flag set
+#define JIE  0x71   //jumps to address if zero flag set
 #define JNE  0x72   //jumps to address if zero flag not set
 #define JLT  0x7a   //jumps to address if zero flag not set AND sign flag not set
 #define JGT  0x7f   //jumps to address if zero flag not set AND sign flag set
-#define INCA 0xA0   //increment value in Memory address (specified in the next two memory addresses)
-#define DECA 0xA1   //decrement value in Memory address (specified in the next two memory addresses)
-#define INCM 0xAA   //increment value in Memory address (specified in REG_H and REG_L)
-#define DECM 0xAB   //decrement value in Memory address (specified in REG_H and REG_L)
-#define CALL 0xB0   //jumps to address, saves address to return to later
+#define INA  0xA0   //increment value in Memory address (specified in the next two memory addresses)
+#define DEA  0xA1   //decrement value in Memory address (specified in the next two memory addresses)
+#define INM  0xAA   //increment value in Memory address (specified in REG_H and REG_L)
+#define DEM  0xAB   //decrement value in Memory address (specified in REG_H and REG_L)
+#define CAL  0xB0   //jumps to address, saves address to return to later
 #define CAE  0xB1   //jumps to address if zero flag set, saves address to return to later
 #define CNE  0xB2   //jumps to address if zero flag not set, saves address to return to later
 #define CLT  0xBa   //jumps to address if zero flag not set AND sign flag not set, saves address to return to later
 #define CGT  0xBb   //jumps to address if zero flag not set AND sign flag set, saves address to return to later
 #define RET  0xBF   //returns to saved address
 #define INT  0x10   //calls the interrupt in saved in next address
+
+/*
+ * Cx Stack pointer operations
+ * x0 SPR - Stack Pointer Read into ACC
+ * x1 SPW - Stack Pointer write from ACC
+ * x2 SPL - Push literal value onto Stack
+ * x3 SPA - Push value from address onto stack
+ * x4 SBA - Pull (bump) value from stack into address
+ * x5 SPM - Push value from HLREG address onto stack
+ * x6 SBM - Pull (bump) value from stack into HLREG address
+ *
+ * C0 - C6 reserved for stack pointer operations => e.g. C5 = Push value from HLREG address onto stack
+ */
 
 /*
  * ADD AND SUB INSTRUCTION      //CMP is just SUB shush
@@ -45,13 +63,13 @@
  * e.g. 0x8c = SUB A,E = subtract the value in E from the value in A
 */
 
-
 /*
  * MOV INSTRUCTION
  * 3x A || B
  * 4x C || D
  * 5x E || F
  * 6x H || L
+ *
  * A x0 || x8
  * B x1 || x9
  * C x2 || xa
@@ -68,6 +86,7 @@
 /*
  * INC AND DEC INSTRUCTIONS
  * 9x INC || DEC
+ *
  * A  x0  || x8
  * B  x1  || x9
  * C  x2  || xa
@@ -81,8 +100,42 @@
  * e.g. 0x94 = INC E = Increment the value in Register E (by one)
  */
 
-unsigned int RetPC = 0;
-unsigned int ProgramCounter = 0;
+/*
+ * REGISTER MOV INSTRUCTIONS
+ * Cx     || MIL
+ * Dx MVO || MVI
+ * Ex MOM || MIM
+ *
+ * A  x0  || x8
+ * B  x1  || x9
+ * C  x2  || xa
+ * D  x3  || xb
+ * E  x4  || xc
+ * F  x5  || xd
+ * H  x6  || xe
+ * L  x7  || xf
+ *
+ * 0xC8 - 0xEf reserved for REGMOV instructions => e.g. 0xCB = move literal (in next address) into REG_D
+ */
+
+/*
+ * SPR AND SBR INSTRUCTIONS
+ * Fx SPR || SBR
+ *
+ * A   x0 || x8
+ * B   x1 || x9
+ * C   x2 || xa
+ * D   x3 || xb
+ * E   x4 || xc
+ * F   x5 || xd
+ * H   x6 || xe
+ * L   x7 || xf
+ *
+ * 0xF0 - 0xFF reserved for SPR and SBR => e.g. 0xFC = Pull (bump) value from stack into REG_E
+ */
+
+unsigned char StackPointer = STACKSIZE-1; //0x0000 - STACKSIZE reserved for stack
+unsigned int ProgramCounter = ENTRYPOINT; //starts at ENTRYPOINT
 unsigned char ACC = 0;
 unsigned char REG_B = 0;
 unsigned char REG_C = 0;
@@ -103,17 +156,17 @@ unsigned char REG_F = 0;
 unsigned char REG_H = 0;    //Memory Register High XX00
 unsigned char REG_L = 0;    //Memory Register Low  00XX
 
-//TODO: MVI, MVO, Implement Stack, IO, Debug mode, Assembler => Keyboard interrupt
-//(CMP could just be a duplicate of SUB, flags are always filled accordingly so JE, JNE, etc. can use them)
+//TODO: Interrupts + IO, Debug mode, Assembler => e.g. Keyboard interrupt
+//(CMP could just be a duplicate of SUB, flags are always filled accordingly so JIE, JNE, etc. can use them)
 
-unsigned char RAM[MEMSIZE];
+unsigned char MEM[MEMSIZE];
 
 void printMEM(){
     printf("--------------------------------------------------------\n");
     printf("      00 01 02 03  04 05 06 07  08 09 0a 0b  0c 0d 0e 0f\n");
 
     for(int ADDR = 0x00; ADDR <= (MEMSIZE-0x10); ADDR += 0x10){
-        printf("%04x  %02x %02x %02x %02x  %02x %02x %02x %02x  %02x %02x %02x %02x  %02x %02x %02x %02x\n", ADDR, RAM[ADDR+0x00], RAM[ADDR+0x01], RAM[ADDR+0x02], RAM[ADDR+0x03], RAM[ADDR+0x04], RAM[ADDR+0x05], RAM[ADDR+0x06], RAM[ADDR+0x07], RAM[ADDR+0x08], RAM[ADDR+0x09], RAM[ADDR+0x0a], RAM[ADDR+0x0b], RAM[ADDR+0x0c], RAM[ADDR+0x0d], RAM[ADDR+0x0e], RAM[ADDR+0x0f]);
+        printf("%04x  %02x %02x %02x %02x  %02x %02x %02x %02x  %02x %02x %02x %02x  %02x %02x %02x %02x\n", ADDR, MEM[ADDR + 0x00], MEM[ADDR + 0x01], MEM[ADDR + 0x02], MEM[ADDR + 0x03], MEM[ADDR + 0x04], MEM[ADDR + 0x05], MEM[ADDR + 0x06], MEM[ADDR + 0x07], MEM[ADDR + 0x08], MEM[ADDR + 0x09], MEM[ADDR + 0x0a], MEM[ADDR + 0x0b], MEM[ADDR + 0x0c], MEM[ADDR + 0x0d], MEM[ADDR + 0x0e], MEM[ADDR + 0x0f]);
     }
     printf("--------------------------------------------------------\n");
 }
@@ -122,7 +175,7 @@ void printDebugInfo(){
     printf("\n========================================================\n");
     printf("EXECUTION HALTED: DUMPING ALL INFO:\n");
     //print all registers, flags, etc. as well
-    printf("program counter at position %04x\n",ProgramCounter);
+    printf("program counter at position %04x\nstack pointer at position %04x\n",ProgramCounter,StackPointer);
     printf("value in Accumulator: %02x\nvalue in Register B:  %02x\n"
            "value in Register C:  %02x\nvalue in Register D:  %02x\nvalue in Register E:  %02x\n"
            "value in Register F:  %02x\nvalue in Register H:  %02x\nvalue in Register L:  %02x\n",
@@ -130,6 +183,29 @@ void printDebugInfo(){
     printf("dumping Memory:\n");
     printMEM();
     printf("========================================================\n");
+}
+
+void PushOnStack(unsigned char value){
+    MEM[StackPointer] = value;
+    if(StackPointer == 0){
+        StackPointer = STACKSIZE-1;
+        printf("STACKOVERFLOW OCCURED\n");
+    }
+    else{
+        StackPointer--;
+    }
+}
+
+unsigned char PullFromStack(){
+    unsigned char value = MEM[StackPointer];
+    if(StackPointer == STACKSIZE-1){
+        StackPointer = 0;
+        printf("STACKOVERFLOW OCCURED\n");
+    }
+    else{
+        StackPointer++;
+    }
+    return value;
 }
 
 unsigned char * getREGTwo(char digitTwo){
@@ -311,7 +387,92 @@ int INCDEC(unsigned char OP){
     return 0;
 }
 
-int getInstruction(unsigned char OP){
+int StackOP(unsigned char OP, unsigned int MEMADDR, unsigned int HLADDR, unsigned char nextLIT){
+    char digitOne = (OP/0x10)*0x10;
+    char digitTwo = OP-(digitOne);
+
+    switch (digitTwo) {
+        case 0x0:
+            ACC = StackPointer;
+            break;
+        case 0x1:
+            StackPointer = ACC;
+            break;
+        case 0x2:
+            PushOnStack(nextLIT);
+            ProgramCounter++;
+            break;
+        case 0x3:
+            PushOnStack(MEM[MEMADDR]);
+            ProgramCounter+=2;
+            break;
+        case 0x4:
+            MEM[MEMADDR] = PullFromStack();
+            ProgramCounter+=2;
+            break;
+        case 0x5:
+            PushOnStack(MEM[HLADDR]);
+            break;
+        case 0x6:
+            MEM[HLADDR] = PullFromStack();
+            break;
+    }
+    return 0;
+}
+
+int REGMOV(unsigned char OP, unsigned int MEMADDR, unsigned int HLADDR, unsigned char nextLIT){
+    unsigned char digitOne = (OP/0x10)*0x10;
+    char digitTwo = OP-(digitOne);
+    int isTwoHigh = 0;
+    if(digitTwo>0x07){
+        isTwoHigh = 1;
+    }
+
+    unsigned char *REG = getREGTwo(digitTwo);
+    if(digitOne == 0xC0){
+        *REG = nextLIT;
+        ProgramCounter++;
+    }
+    else if(digitOne == 0xD0){
+        if(isTwoHigh){
+            *REG = MEM[MEMADDR];
+        }
+        else{
+            MEM[MEMADDR] = *REG;
+        }
+        ProgramCounter+=2;
+    }
+    else if(digitOne == 0xE0){
+        if(isTwoHigh){
+            *REG = MEM[HLADDR];
+        }
+        else{
+            MEM[HLADDR] = *REG;
+        }
+        ProgramCounter+=2;
+    }
+    return 0;
+}
+
+int SOPREG(unsigned char OP, unsigned int MEMADDR, unsigned int HLADDR, unsigned char nextLIT){
+    char digitOne = (OP/0x10)*0x10;
+    char digitTwo = OP-(digitOne);
+    int isTwoHigh = 0;
+    if(digitTwo>0x07){
+        isTwoHigh = 1;
+    }
+
+    unsigned char *REG = getREGTwo(digitTwo);
+    if(isTwoHigh){
+        *REG = PullFromStack();
+    }
+    else{
+        PushOnStack(*REG);
+    }
+    return 0;
+}
+
+int getInstruction(unsigned char OP, unsigned int MEMADDR, unsigned int HLADDR, unsigned char nextLIT){
     if(OP >= 0x30 && OP <= 0x6f){
         return MOV(OP);
     }
@@ -321,11 +482,36 @@ int getInstruction(unsigned char OP){
     else if (OP >= 0x90 && OP <= 0x9f){
         return INCDEC(OP);
     }
-    return 1;
+    else if (OP >= 0xC0 && OP <= 0xC6){
+        return StackOP(OP, MEMADDR, HLADDR, nextLIT);
+    }
+    else if (OP >= 0xC8 && OP <= 0xEF){
+        return REGMOV(OP, MEMADDR, HLADDR, nextLIT);
+    }
+    else if (OP >= 0xF0 && OP <= 0xFF){
+        return SOPREG(OP, MEMADDR, HLADDR, nextLIT);
+    }
+    else {
+        return 1;
+    }
 }
 
 void HandleBIOSInterrupt(unsigned char INTCODE){
     //set ProgramCounter to Address that corresponds to interrupt handling code...
+}
+
+void Push16bitValInStack(unsigned int value){
+    unsigned char HighByte = value/0x100;
+    unsigned char LowByte = value-HighByte;
+    PushOnStack(LowByte);
+    PushOnStack(HighByte);
+}
+
+unsigned int Pull16bitValFromStack(){
+    unsigned char LowByte = PullFromStack();
+    unsigned char HighByte = PullFromStack();
+    unsigned int value = HighByte*0x100+LowByte;
+    return value;
 }
 
 int MainLoop(){
@@ -334,9 +520,9 @@ int MainLoop(){
         if(INTCODE != 0x00){
             HandleBIOSInterrupt(INTCODE);
         }
-        unsigned char OP = RAM[ProgramCounter];
+        unsigned char OP = MEM[ProgramCounter];
         unsigned int HLADDR = REG_H*0x100+REG_L;
-        unsigned int MEMADDR = RAM[ProgramCounter+1]*0x100+RAM[ProgramCounter+2];
+        unsigned int MEMADDR = MEM[ProgramCounter + 1] * 0x100 + MEM[ProgramCounter + 2];
         switch(OP){
             case NOP:
                 break;
@@ -344,31 +530,31 @@ int MainLoop(){
                 printDebugInfo();
                 return 0;
             case INT:
-                HandleCPUInterrupt(RAM[ProgramCounter+1],ACC);
+                HandleCPUInterrupt(MEM[ProgramCounter + 1], ACC);
                 ProgramCounter++;
                 break;
-            case LDAL:
-                ACC = RAM[ProgramCounter+1];
+            case LDL:
+                ACC = MEM[ProgramCounter + 1];
                 ProgramCounter++;
                 break;
             case LDA:
-                ACC = RAM[MEMADDR];
+                ACC = MEM[MEMADDR];
                 ProgramCounter+=2;
                 break;
             case STA:
-                RAM[MEMADDR] = ACC;
+                MEM[MEMADDR] = ACC;
                 ProgramCounter+=2;
                 break;
-            case LDAM:
-                ACC = RAM[HLADDR];
+            case LDM:
+                ACC = MEM[HLADDR];
                 break;
-            case STAM:
-                RAM[HLADDR] = ACC;
+            case STM:
+                MEM[HLADDR] = ACC;
                 break;
             case JMP:
-            case CALL:
-                if(OP == CALL){
-                    RetPC = ProgramCounter+2;
+            case CAL:
+                if(OP == CAL){
+                    Push16bitValInStack(ProgramCounter+2);
                 }
                 ProgramCounter = MEMADDR-1;
                 break;
@@ -376,7 +562,7 @@ int MainLoop(){
             case CNE:
                 if(!(REG_F & 0b01000000)){
                     if(OP == CNE){
-                        RetPC = ProgramCounter+2;
+                        Push16bitValInStack(ProgramCounter+2);
                     }
                     ProgramCounter = MEMADDR-1;
                 }
@@ -384,11 +570,11 @@ int MainLoop(){
                     ProgramCounter+=2;
                 }
                 break;
-            case JE:
+            case JIE:
             case CAE:
                 if(REG_F & 0b01000000){
                     if(OP == CAE){
-                        RetPC = ProgramCounter+2;
+                        Push16bitValInStack(ProgramCounter+2);
                     }
                     ProgramCounter = MEMADDR-1;
                 }
@@ -400,7 +586,7 @@ int MainLoop(){
             case CGT:
                 if((REG_F & 0b01000000) && (REG_F & 0b10000000)){
                     if(OP == CGT){
-                        RetPC = ProgramCounter+2;
+                        Push16bitValInStack(ProgramCounter+2);
                     }
                     ProgramCounter = MEMADDR-1;
                 }
@@ -412,7 +598,7 @@ int MainLoop(){
             case CLT:
                 if((REG_F & 0b01000000) && !(REG_F & 0b10000000)){
                     if(OP == CLT){
-                        RetPC = ProgramCounter+2;
+                        Push16bitValInStack(ProgramCounter+2);
                     }
                     ProgramCounter = MEMADDR-1;
                 }
@@ -421,64 +607,65 @@ int MainLoop(){
                 }
                 break;
             case RET:
-                ProgramCounter = RetPC;
+                ProgramCounter = Pull16bitValFromStack();
                 break;
-            case INCA:
-                if(RAM[MEMADDR] == 0xFF){
-                    RAM[MEMADDR] = 0x00;
+            case INA:
+                if(MEM[MEMADDR] == 0xFF){
+                    MEM[MEMADDR] = 0x00;
                     REG_F |= 0b00000001;
                 }
                 else{
-                    RAM[MEMADDR] += 1;
+                    MEM[MEMADDR] += 1;
                 }
-                setParity(&RAM[MEMADDR]);
+                setParity(&MEM[MEMADDR]);
                 ProgramCounter+=2;
                 break;
-            case DECA:
-                if(RAM[MEMADDR] == 0x00){
-                    RAM[MEMADDR] += 1;
+            case DEA:
+                if(MEM[MEMADDR] == 0x00){
+                    MEM[MEMADDR] += 1;
                     REG_F &= 0b01111111;
                 }
                 else{
-                    RAM[MEMADDR] -=1;
-                    if(RAM[MEMADDR] == 0x00){
+                    MEM[MEMADDR] -=1;
+                    if(MEM[MEMADDR] == 0x00){
                         REG_F |= 0b01000000;
                     }
                     else{
                         REG_F &= 0b10111111;
                     }
                 }
-                setParity(&RAM[MEMADDR]);
+                setParity(&MEM[MEMADDR]);
                 ProgramCounter+=2;
                 break;
-            case INCM:
-                if(RAM[HLADDR] == 0xFF){
-                    RAM[HLADDR] = 0x00;
+            case INM:
+                if(MEM[HLADDR] == 0xFF){
+                    MEM[HLADDR] = 0x00;
                     REG_F &= 0b00000001;
                 }
                 else{
-                    RAM[HLADDR] += 1;
+                    MEM[HLADDR] += 1;
                 }
-                setParity(&RAM[HLADDR]);
+                setParity(&MEM[HLADDR]);
                 break;
-            case DECM:
-                if(RAM[HLADDR] == 0x00){
-                    RAM[HLADDR] += 1;
+            case DEM:
+                if(MEM[HLADDR] == 0x00){
+                    MEM[HLADDR] += 1;
                     REG_F &= 0b01111111;
                 }
                 else{
-                    RAM[HLADDR] -=1;
-                    if(RAM[HLADDR] == 0x00){
+                    MEM[HLADDR] -=1;
+                    if(MEM[HLADDR] == 0x00){
                         REG_F |= 0b01000000;
                     }
                     else{
                         REG_F &= 0b10111111;
                     }
                 }
-                setParity(&RAM[HLADDR]);
+                setParity(&MEM[HLADDR]);
                 break;
             default:
-                if(getInstruction(OP)){
+
+                if(getInstruction(OP,MEMADDR,HLADDR,MEM[ProgramCounter+1])){
                     printf("ERROR: invalid opcode detected, cannot execute");
                     printDebugInfo();
                     return 1;
@@ -496,7 +683,7 @@ int main() {
         printf("FILE READ ERROR\n");
         return 1;
     }
-    fread(RAM, sizeof(RAM), 1, src);
+    fread(MEM, sizeof(MEM), 1, src);
 
     int ret = MainLoop();
 
